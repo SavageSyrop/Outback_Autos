7be35986fbf33d677d8744c25bb54ba05fc05f1b
SavageSyrop <fess.2002@mail.ru>
2022-05-25 19:38:52 +0300

    Speed tab

---
+++ app/src/main/java/com/example/outback_autos/HowToUseActivity.java
@@ -0,0 +1,34 @@
+package com.example.outback_autos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class HowToUseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_use);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.htu_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
